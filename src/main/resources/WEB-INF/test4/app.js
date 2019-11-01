4564a70a05c934286a9265f3829a08f4da8c6a4f
sedan7001 <sedan7001@gmail.com>
2019-11-01 21:13:44 +0900

    ng build 후 번들 인스톨

--- src/main/resources/WEB-INF/test4/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	app.register.controller('HelloAppController', function HelloAppController($scope) {
-	  // code here
-	});
-})();
- 
-//# sourceURL=apps/test4/test4/app.js
--- src/main/resources/WEB-INF/test4/config.js
+++
@@ -1,9 +0,0 @@
-app.config(function($stateProvider, $urlRouterProvider) {
- 
-});
- 
-app.run(function($translatePartialLoader) {
-	$translatePartialLoader.addPart('/apps/test4/test4/locales');
-});
- 
-//# sourceURL=apps/test4/test4/config.js
