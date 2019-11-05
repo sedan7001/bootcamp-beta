acb45513ba4a27b5e2aaa0a695a5bc89404c5d81
sedan7001 <sedan7001@gmail.com>
2019-11-05 15:21:53 +0900

    앱 컴포넌트 쿼리함수명 변경, 앱컴포넌트 스타일 변경

--- src/main/test4/src/app/app.component.ts
+++ src/main/test4/src/app/app.component.ts
@@ -7,23 +7,17 @@ import { QueryService } from 'eediom-sdk';
 })
 export class AppComponent {
   title = 'test4';
-  // query = "bootcamp name=test query='search index=github' | parse event";
-  // query = "table sys_cpu_logs";
-  query;
-  result;
+  query:string = '';
+  result:any = [];
+  runQuery:boolean = false;
 
   constructor(private queryService: QueryService) {
   }
 
-  ngOnInit(): void {
-    // this.init();
-  }
-
-
-  async init(): Promise<void> {
+  executeQuery() {
     this.queryService.query(this.query, 100, 0).then((res) => {
-      console.log(res);
       this.result = res.records;
+      this.runQuery = true;
     });
   }
 }
