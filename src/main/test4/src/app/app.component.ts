6563c6e1b62f9777af3574638f1b94554586daf7
sedan7001 <sedan7001@gmail.com>
2019-11-11 11:17:47 +0900

    eediom-sdk변경으로 쿼리서비스 실행부분 변경.

--- src/main/test4/src/app/app.component.ts
+++ src/main/test4/src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
-import { QueryService } from 'eediom-sdk';
+import { Component, NgZone } from '@angular/core';
+import { QueryService, SubscribeTypes } from 'eediom-sdk';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'test4';
+  title = 'bootcamp';
   query:string = '';
-  result:any = [];
+  private _result: any = [];
+  public get result(): any {
+    return this._result;
+  }
+  public set result(value: any) {
+    this._result = value;
+  }
   runQuery:boolean = false;
 
-  constructor(private queryService: QueryService) {
+  constructor(private queryService: QueryService, private ngZone: NgZone) {
   }
 
   executeQuery() {
-    this.queryService.query(this.query, 100, 0).then((res) => {
-      this.result = res.records;
-      this.runQuery = true;
+    this.queryService.query(this.query, (queryId, subscribeData) => {
+      if (subscribeData.type === SubscribeTypes.Eof) {
+        this.queryService.getResult(queryId, 100, 0).then((queryResult) => {
+          this.ngZone.run(() => {
+            this.runQuery = true;
+            this.result = queryResult.records;
+          })
+        })
+      }
     });
   }
 }
